d71b396325413ac77c675ff37c096933eb31020e
PastingZ <a20234068@pucp.edu.pe>
2026-04-22 01:52:24 -0500

    XD

--- gigachadsys-dao/src/main/java/pe/edu/pucp/gigachadsys/dao/impl/clases/ClaseGrupalDAOImpl.java
+++ gigachadsys-dao/src/main/java/pe/edu/pucp/gigachadsys/dao/impl/clases/ClaseGrupalDAOImpl.java
@@ -1,6 +1,35 @@
 package pe.edu.pucp.gigachadsys.dao.impl.clases;
 
-import pe.edu.pucp.gigachadsys.dao.ClaseGrupalDAO;
+import pe.edu.pucp.gigachadsys.inter.clases.ClaseGrupalDAO;
+import pe.edu.pucp.gigachadsys.model.ClaseGrupal;
+
+import java.util.List;
 
 public class ClaseGrupalDAOImpl implements ClaseGrupalDAO {
+
+
+    @Override
+    public List<ClaseGrupal> listAll() {
+        return List.of();
+    }
+
+    @Override
+    public ClaseGrupal load(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public ClaseGrupal save(ClaseGrupal claseGrupal) {
+        return null;
+    }
+
+    @Override
+    public ClaseGrupal update(ClaseGrupal claseGrupal) {
+        return null;
+    }
+
+    @Override
+    public void remove(ClaseGrupal claseGrupal) {
+
+    }
 }
