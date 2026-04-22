74dd78e0b6db7d468379a9ce2ea3ea3b7a723f17
PastingZ <a20234068@pucp.edu.pe>
2026-04-22 00:56:13 -0500

    codigo antes de sincronizar

--- gigachadsys-dao/src/main/java/pe/edu/pucp/gigachadsys/dao/SuscripcionDAO.java
+++ gigachadsys-dao/src/main/java/pe/edu/pucp/gigachadsys/dao/SuscripcionDAO.java
@@ -1,8 +1,4 @@
-<<<<<<<< Updated upstream:gigachadsys-dao/src/main/java/pe/edu/pucp/gigachadsys/SuscripcionDAO.java
-package pe.edu.pucp.gigachadsys;
-========
-package pe.edu.pucp.gigachadsys.dao;
->>>>>>>> Stashed changes:gigachadsys-dao/src/main/java/pe/edu/pucp/gigachadsys/dao/SuscripcionDAO.java
+
 
 import pe.edu.pucp.gigachadsys.dao.base.BaseDAO;
 import pe.edu.pucp.gigachadsys.model.Suscripcion;
